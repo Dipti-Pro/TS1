0aa3813054d62fb4e6b694d43313359ed98b366a
Dipti-Pro <85604091+Dipti-Pro@users.noreply.github.com>
2021-08-15 11:38:15 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,30 +18,18 @@ function setup() {
   world = engine.world;
   Engine.run(engine);
   ground = new Ground();
-  stand1 = new Stand(390,300,250,10);
-  stand2 = new Stand(700,200,200,10);
+  //make the stands
+  
  
   //level one
-  block1 = new Block(300,275,30,40);
-  console.log(block1);
-  block2 = new Block(330,275,30,40);
-  block3 = new Block(360,275,30,40);
-  block4 = new Block(390,275,30,40);
-  block5 = new Block(420,275,30,40);
-  block6 = new Block(450,275,30,40);
-  block7 = new Block(480,275,30,40);
+  //make 7 blocks
+ ]
   //level two
-  block8 = new Block(330,235,30,40);
-  block9 = new Block(360,235,30,40);
-  block10 = new Block(390,235,30,40);
-  block11 = new Block(420,235,30,40);
-  block12 = new Block(450,235,30,40);
+//block 8 to 12
+ 
   //level three
-  block13 = new Block(360,195,30,40);
-  block14 = new Block(390,195,30,40);
-  block15 = new Block(420,195,30,40);
-  //top
-  block16 = new Block(390,155,30,40);
+ //lock13 to 16
+ 
 
   //set 2 for second stand
   //level one
@@ -116,9 +104,4 @@ function draw() {
 
   slingShot.display();
 }
-function mouseDragged(){
-  Matter.Body.setPosition(this.ball,{x:mouseX,y:mouseY});
-}
-function mouseReleased(){
-  slingShot.fly();
-}
+//function mouse dragged and realeased
